e5cc9743127d7452e245d37bbb788959e443fa66
Marco Lotz <marco@marcolotz.com>
2024-04-13 10:34:51 +0200

    adding first files

---
+++ src/main/java/io/neucom/cryptostreams/CryptoStreamsApplication.java
@@ -0,0 +1,13 @@
+package io.neucom.cryptostreams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoStreamsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoStreamsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/neucom/cryptostreams/CryptoStreamsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.neucom.cryptostreams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoStreamsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
